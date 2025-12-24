955a581c15d223ca545c866ed46c4ba4ae4b2ee8
adarshg-org-13 <https://github.com/adarshg-org-13/Whispers-of-the-pertrified-path>
2025-12-24 14:59:20 +0530

    fixed all the complications

--- Scripts/game_main_menu.gd
+++ Scripts/game_main_menu.gd
@@ -10,16 +10,16 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	pass
 
+
 func _on_start_game_pressed() -> void:
 	pass # Replace with function body.
 	get_tree().change_scene_to_file("res://main.tscn")
-	
+
+
 func _on_settings_pressed() -> void:
-	pass 
+	pass # Replace with function body.
 	get_tree().change_scene_to_file("res://settings_menu.tscn")
-	
 	
 func _on_quit_game_pressed() -> void:
 	pass # Replace with function body.
 	get_tree().quit()
-	
